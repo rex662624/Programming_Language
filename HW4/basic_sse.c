ce9c60d6a3e12367f7a93fe71dbecabfde3f7bcd
rex662624 <zfk.662624@gmail.com>
2018-06-11 19:30:14 +0800

    Complete all work

--- HW4/basic_sse.c
+++ HW4/basic_sse.c
@@ -4,16 +4,17 @@
 #include <omp.h>
 #include <string.h>
 #include <x86intrin.h>
+#include <immintrin.h>
 
 #define INPUTFILE "input/test2"
 
-void malloc_matrix(int m, int n, float ***matptr);
-void multiply(int m1, int n1, float **mat1, int m2, int n2, float **mat2, float **mat3);
-void multiply_NotP(int m1, int n1, float **mat1, int m2, int n2, float **mat2,float **mat3);
+void malloc_matrix(int m, int n, double ***matptr);
+void multiply(int m1, int n1, double **mat1, int m2, int n2, double **mat2, double **mat3);
+void multiply_NotP(int m1, int n1, double **mat1, int m2, int n2, double **mat2,double **mat3);
 
 int to_x_multiple(int x, int in);
 int thread_count=1;
-float **A, **B, **C;
+double **A, **B, **C;
 
 int main(int argc,char*argv []) {
 	//輸入的
@@ -47,7 +48,7 @@ int main(int argc,char*argv []) {
 	
 	for(i = 0; i < ma_in; i++)	{
 		for(j = 0; j < na_in; j++) {
-				fscanf(infile, "%f", &A[i][j]);
+				fscanf(infile, "%lf", &A[i][j]);
 		}
 	}
 
@@ -64,7 +65,7 @@ int main(int argc,char*argv []) {
      	malloc_matrix(mb, nb, &B);
 	for(i = 0; i < mb_in; i++)	{
 		for(j = 0; j < nb_in; j++) {
-				fscanf(infile, "%f", &B[i][j]);
+				fscanf(infile, "%lf", &B[i][j]);
 		}
 	}
 	
@@ -79,12 +80,15 @@ int main(int argc,char*argv []) {
 	thread_count = strtol(argv[1],NULL,10);
 
 	//not parallel
+
 	double st=omp_get_wtime();
 	multiply_NotP(ma, na, A, mb, nb, B, C);
 	double en=omp_get_wtime();
 	printf("Basic + SSE: %lf\n",en-st);	
 	fprintf(outfile, "Basic + SSE: %lf s \n", en-st);
+
 	//parallel
+
 	st=omp_get_wtime();
 	multiply(ma, na, A, mb, nb, B, C);
 	en=omp_get_wtime();
@@ -111,21 +115,21 @@ int to_x_multiple(int x, int in) {
 	return in;
 }
 
-void malloc_matrix(int m, int n, float ***matptr) {
+void malloc_matrix(int m, int n, double ***matptr) {
 	// the memory of the 2d array is consecutive (each row)
 	int i;
-	float *tmp;	
-	tmp = _mm_malloc(m * n * sizeof(float), 16);
-//	tmp = malloc(m * n * sizeof(float));
-	memset(tmp, 0, m*n * sizeof (float));
-	*matptr = malloc(m * sizeof(float *));
+	double *tmp;	
+	tmp = _mm_malloc(m * n * sizeof(double), 64);
+//	tmp = malloc(m * n * sizeof(double));
+	memset(tmp, 0, m*n * sizeof (double));
+	*matptr = malloc(m * sizeof(double *));
 	for(i = 0; i < m; i++)
 		(*matptr)[i] = &(tmp[n*i]);
 	
 }
 
 
-void multiply(int m1, int n1, float **mat1, int m2, int n2, float **mat2, float **mat3) {	
+void multiply(int m1, int n1, double **mat1, int m2, int n2, double **mat2, double **mat3) {	
 	int i, j, k;
 
 #pragma omp parallel for num_threads(thread_count)private(i,j) shared(mat3)
@@ -135,32 +139,32 @@ void multiply(int m1, int n1, float **mat1, int m2, int n2, float **mat2, float 
 	#pragma omp parallel for num_threads(thread_count) private(i, j, k) shared(mat3,mat1,mat2,m1,n1,n2)
     for(i=0; i<m1; i++) {
         for(j=0; j<n1; j++) {
-            __m128 a4 = _mm_load1_ps(&mat1[i][j]);
+            __m256d a4 = _mm256_set_pd(mat1[i][j],mat1[i][j],mat1[i][j],mat1[i][j]);
             for(k=0; k<n2; k+=4) {
-                __m128 c4 = _mm_load_ps(&mat3[i][k]);
-                __m128 b4 = _mm_load_ps(&mat2[j][k]);
-                c4 = _mm_add_ps(_mm_mul_ps(a4,b4),c4);
-                _mm_store_ps(&mat3[i][k], c4);
+                __m256d c4 = _mm256_load_pd(&mat3[i][k]);
+                __m256d b4 = _mm256_load_pd(&mat2[j][k]);
+                c4 = _mm256_add_pd(_mm256_mul_pd(a4,b4),c4);
+                _mm256_store_pd(&mat3[i][k], c4);
             }
         }
     }
 }
 
-void multiply_NotP(int m1, int n1, float **mat1, int m2, int n2, float **mat2, float **mat3) {	
+void multiply_NotP(int m1, int n1, double **mat1, int m2, int n2, double **mat2, double **mat3) {	
 	int i, j, k;
 
 	for(i = 0; i < m1; i++)
 		for(j = 0; j < n2; j++)
 			mat3[i][j] = 0;	
 	
-for(i=0; i<m1; i++) {
+	for(i=0; i<m1; i++) {
         for(j=0; j<n1; j++) {
-            __m128 a4 = _mm_load1_ps(&mat1[i][j]);
+            __m256d a4 = _mm256_set_pd(mat1[i][j],mat1[i][j],mat1[i][j],mat1[i][j]);
             for(k=0; k<n2; k+=4) {
-                __m128 c4 = _mm_load_ps(&mat3[i][k]);
-                __m128 b4 = _mm_load_ps(&mat2[j][k]);
-                c4 = _mm_add_ps(_mm_mul_ps(a4,b4),c4);
-                _mm_store_ps(&mat3[i][k], c4);
+                __m256d c4 = _mm256_load_pd(&mat3[i][k]);
+                __m256d b4 = _mm256_load_pd(&mat2[j][k]);
+                c4 = _mm256_add_pd(_mm256_mul_pd(a4,b4),c4);
+                _mm256_store_pd(&mat3[i][k], c4);
             }
         }
     }
